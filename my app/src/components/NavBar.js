2eecbe87a7fba15641aa7677990355a0eb411c35
Aftermath001 <alvinasiachi@gmail.com>
2024-04-30 22:59:08 +0300

    Dashboard Styling

--- my app/src/components/NavBar.js
+++ my app/src/components/NavBar.js
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import logo from '../images/LogoCRM.jpg'
+import {useNavigate} from 'react-router-dom'
 const NavBar = () => {
+  
+  const navigate =useNavigate()
+
   return (
-    <div>NavBar</div>
+    <nav>
+      <div className="logo-container">
+        <img src ={logo} alt="LOGO"/>
+      </div>
+      <div className="controls-container">
+        <div className="icons" onClick ={() => navigate('/ticket')}>+</div>
+        <div className="icons"  onClick ={() => navigate('/')}> ^ </div>
+      </div>
+
+    </nav>
   )
 }
 
--- my app/src/components/TicketCard.js
+++ my app/src/components/TicketCard.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import DeleteBlock from './DeleteBlock'
+import StatusDisplay from './StatusDisplay'
+import ProgressDisplay from './ProgressDisplay'
+import PriorityDisplay from './PriorityDisplay'
+import AvatarDisplay from './AvatarDisplay'
 
 const TicketCard = () => {
   return (
-    <div>TicketCard</div>
+    <div className="ticket-card">
+       <div className="ticket-color">
+        <Link>
+          <h3>Title</h3>
+          <AvatarDisplay/>
+          <StatusDisplay/>
+          <PriorityDisplay/>
+          <ProgressDisplay/>
+        </Link>
+        <DeleteBlock/>
+       </div>
+    </div>
   )
 }
 
