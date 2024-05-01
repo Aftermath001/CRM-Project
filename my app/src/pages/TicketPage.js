7b85c389e7ab630a2f57d4cefeb13d40ca44776a
Aftermath001 <alvinasiachi@gmail.com>
2024-05-01 12:15:18 +0300

    Ticket Page

--- my app/src/pages/TicketPage.js
+++ my app/src/pages/TicketPage.js
@@ -70,6 +70,117 @@ const TicketPage = () => {
                 required={true}
                 value={formData.category}
               />
+              <label>Priority</label>
+              <div className="multiple-input-container">
+                <input
+                 id="priority-1"
+                 name="priority"
+                 type="radio"
+                 onChange={handleChange}
+                 value={1}
+                 checked={formData.priority === 1}
+                />
+                <label htmlFor="priority-1">1</label>
+
+
+                <input
+                 id="priority-2"
+                 name="priority"
+                 type="radio"
+                 onChange={handleChange}
+                 value={2}
+                 checked={formData.priority === 2}
+                />
+                <label htmlFor="priority-2">2</label>
+
+                <input
+                 id="priority-3"
+                 name="priority"
+                 type="radio"
+                 onChange={handleChange}
+                 value={3}
+                 checked={formData.priority === 3}
+                />
+                <label htmlFor="priority-3">3</label>
+
+                <input
+                 id="priority-4"
+                 name="priority"
+                 type="radio"
+                 onChange={handleChange}
+                 value={4}
+                 checked={formData.priority === 4}
+                />
+                <label htmlFor="priority-4">4</label>
+
+                <input
+                 id="priority-5"
+                 name="priority"
+                 type="radio"
+                 onChange={handleChange}
+                 value={5}
+                 checked={formData.priority === 5}
+                />
+                <label htmlFor="priority-5">5</label>
+             </div>
+
+
+            {editMode && 
+                <>
+                  <input
+                  type="range"
+                  id="progress"
+                  name="progress"
+                  value={formData.progress}
+                  min="0"
+                  max="100"
+                  onChange={handleChange}
+                />
+                <label htmlFor="progress">Progress</label>
+               
+
+                <label>Status</label>
+                <select 
+                  name="status"
+                  value={formData.status}
+                  onChange={handleChange}
+                >
+                  <option selected={formData.status === 'done'} value='done'>Done</option>
+                  <option selected={formData.status === 'working on it'} value='working on it'>Working on it</option>
+                  <option selected={formData.status === 'stuck'} value='stuck'>Stuck</option>
+                  <option selected={formData.status === 'not started'} value='not started'>Not Started</option>
+
+                </select>
+                </>
+            }
+            <input type="submit"/>
+            </section>
+
+            <section>
+              <label htmlFor="owner">Owner</label>
+              <input
+                id="owner"
+                name="owner"
+                type="text"
+                onChange={handleChange}
+                required={true}
+                value={formData.owner}
+              />
+
+              <label htmlFor="avatar">Avatar</label>
+              <input
+                id="avatar"
+                name="avatar"
+                type="text"
+                onChange={handleChange}
+                required={true}
+                value={formData.avatar}
+              />
+              <div className="img-preview">
+                {formData.avatar && (
+                  <img src={formData.avatar} alt="img-preview"/>
+                )}
+              </div>
             </section>
         </form>
       </div>
